a84c4ea96725a124ed7690f0370c2521b61e19d6
dev-bars <renan.br3@gmail.com>
2025-11-05 21:14:05 -0300

    commit - importar somente o que foi usado do framework

--- aula06/baskaraST.py
+++ aula06/baskaraST.py
@@ -1,29 +1,29 @@
-import streamlit as st 
-import math as mt
-st.header("Calculadora de Bhaskara")
-st.write("Calculadora de raízes \n de uma equação do segundo grau")
-st.write("ax² + bx + c = 0")
+from streamlit import header,write,text_input,button,warning,success,error
+from math import sqrt,pow
+header("Calculadora de Bhaskara")
+write("Calculadora de raízes \n de uma equação do segundo grau")
+write("ax² + bx + c = 0")
 
 # Entrada de dados
-a = st.text_input("Insira o valor de A:")
-b = st.text_input("Insira o valor de B:")
-c = st.text_input("Insira o valor de C:")
+a = text_input("Insira o valor de A:")
+b = text_input("Insira o valor de B:")
+c = text_input("Insira o valor de C:")
 
 # Processamento de dados
-if st.button("Calcular raizes"):
+if button("Calcular raizes"):
     try:
         a = float(a)
         b = float(b)
         c = float(c)
-        delta = b**2 -4*a*c
+        delta = pow(b,2) -4 * a * c
         if delta < 0:
-            st.warning(f"A equação possui uma raiz real: {raiz}")
+            warning(f"A equação não possui raizes reais")
         elif delta == 0:
-            raiz = (-b + mt.sqrt(delta)) / (2*a)
-            st.success(f"As raízes da equação são \n Raiz 1: {raiz1} \n Raiz 2: {raiz2}")
+            raiz = (-b + sqrt(delta)) / (2*a)
+            success(f"A equção possui uma raiz real {raiz}")
         else:
-            raiz1 = (-b + mt.sqrt(delta)) / (2*a)
-            raiz2 = (-b + mt.sqrt(delta)) / (2*a)
-            st.success(f"As raizes da equação são \n Raiz 1:{raiz1}, \n Raiz2: {raiz2}")
+            raiz1 = (-b + sqrt(delta)) / (2*a)
+            raiz2 = (-b + sqrt(delta)) / (2*a)
+            success(f"As raizes da equação são \n Raiz 1:{raiz1}, \n Raiz2: {raiz2}")
     except:
-        st.error("Por favor insira valores válidos")
+        error("Por favor insira valores válidos")
