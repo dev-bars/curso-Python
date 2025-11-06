cb8672e915496bd7fc63d3b5b38db65c901920b0
dev-bars <renan.br3@gmail.com>
2025-11-05 21:43:08 -0300

    commit - funçao - maior legibilidade  para manutenção

--- aula06/baskaraST.py
+++ aula06/baskaraST.py
@@ -1,5 +1,11 @@
 from streamlit import header,write,text_input,button,warning,success,error
 from math import sqrt,pow
+
+# Função Python
+def calculo(delta):
+    valor = (sqrt(delta)) / (2*a)
+    return valor
+
 header("Calculadora de Bhaskara")
 write("Calculadora de raízes \n de uma equação do segundo grau")
 write("ax² + bx + c = 0")
@@ -17,13 +23,15 @@ if button("Calcular raizes"):
         c = float(c)
         delta = pow(b,2) -4 * a * c
         if delta < 0:
-            warning(f"A equação não possui raizes reais")
+            warning(f"A equação não possui raizes reais👍")
         elif delta == 0:
-            raiz = (-b + sqrt(delta)) / (2*a)
+            raiz = (-b + calculo(delta)) / (2*a)
             success(f"A equção possui uma raiz real {raiz}")
         else:
-            raiz1 = (-b + sqrt(delta)) / (2*a)
-            raiz2 = (-b + sqrt(delta)) / (2*a)
+            raiz1 = (-b + calculo(delta)) / (2*a)
+            raiz2 = (-b + calculo(delta)) / (2*a)
             success(f"As raizes da equação são \n Raiz 1:{raiz1}, \n Raiz2: {raiz2}")
-    except:
+    except ValueError:
         error("Por favor insira valores válidos")
+    except ZeroDivisionError:
+        error(f"O valor de 'a' não pode ser zero em uma equação do segundo grau. ")
