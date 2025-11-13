2b169a24c6e060f4796441281c4449f2ff726a30
dev-bars <renan.br3@gmail.com>
2025-11-13 20:47:36 -0300

    commit - classe: atributos e metodos

--- oop01/trianguloOOP.py
+++ oop01/trianguloOOP.py
@@ -8,3 +8,8 @@ class triangulo:
     b = 0
     c = 0
     
+    #Métodos
+    def area(self): #Self é um paramettro, aponta a informação que foi inseridos pelo usuario
+        p = (self.a + self.b +self.c) / 2
+        area = (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
+        return area
