946766c00266639cbe16e4fd2ae2d88836683466
dev-bars <renan.br3@gmail.com>
2025-11-14 20:40:57 -0300

    commit - calculadora método classe estática

--- circulo/calculadora.py
+++ circulo/calculadora.py
@@ -1,7 +1,10 @@
 class calculadora1:
+    #Atributo da classe
     PI = 3.14
-
-    def circunferencia(self, raio) -> float:
-        return 2 * self.PI * raio
-    def area(self, raio) -> float:
-        return self.PI * raio ** 2
+    #Método estático de classe
+    @staticmethod
+    def circunferencia(raio) -> float:
+        return 2 * calculadora1.PI * raio
+    @staticmethod
+    def area(raio) -> float:
+        return calculadora1.PI * raio ** 2
--- circulo/circulo1.py
+++ circulo/circulo1.py
@@ -1,14 +1,11 @@
-import calculadora as c
-#Instanciação do objeto
-circulo = c.calculadora1()
+from calculadora import calculadora1
 #Entrada de dados
 raio = float(input("Digite o valor do raio: "))
 #Processamento de dados
-#chamar os métodos
-circunferencia = circulo.circunferencia(raio)
-area = circulo.area(raio)
+circunferencia = calculadora1.circunferencia(raio)
+area = calculadora1.area(raio)
 #Saida de dados
 print(f''' Circunferencia: {circunferencia:.2f}
       Area: {area:.2f}
-      PI: {circulo.PI})
+      PI: {calculadora1.PI})
       ''')
